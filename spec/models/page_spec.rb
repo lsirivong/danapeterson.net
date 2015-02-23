667a9c55dfd993c4f8e555f6b62fe86fbf27a072
Lenny Sirivong <lenny@lenny-s.com>
2015-02-22 22:58:44 -0500

    code style.

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -2,27 +2,27 @@ require 'rails_helper'
 
 RSpec.describe Page, type: :model do
   it "has a valid factory" do
-    page = FactoryGirl.build(:page)
+    page = FactoryGirl.build :page
     expect(page).to be_valid
   end
 
   it "is invalid without a title" do
-    page = FactoryGirl.build(:page, title: nil)
+    page = FactoryGirl.build :page, title: nil
     expect(page).to be_invalid
   end
 
   it "is invalid with an empty title" do
-    page = FactoryGirl.build(:page, title: '')
+    page = FactoryGirl.build :page, title: ''
     expect(page).to be_invalid
   end
 
   it "is invalid without a slug" do
-    page = FactoryGirl.build(:page, slug: nil)
+    page = FactoryGirl.build :page, slug: nil
     expect(page).to be_invalid
   end
 
   it "is invalid with an empty slug" do
-    page = FactoryGirl.build(:page, slug: '')
+    page = FactoryGirl.build :page, slug: ''
     expect(page).to be_invalid
   end
 end
